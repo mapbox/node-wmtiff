a5624e8f94472b78566191f8ee9d4aa5f9248c75
Amit Kapadia <akapad@gmail.com>
2015-01-26 18:01:32 -0800

    test reproject against gdalwarp

--- test/test.js
+++ test/test.js
@@ -6,9 +6,55 @@ var wmtiff = require('../index.js');
 var gdal = require('gdal');
 
 
+tape('setup', function(assert) {
+  
+  var dir = path.join(__dirname, 'fixtures', 'webmercator');
+  fs.mkdir(dir, function(error) {
+    assert.end();
+  });
+  
+});
+
 tape('reproject', function(assert) {
+  
+  var datadir = path.join(__dirname, 'fixtures');
+  fs.readdir(datadir, function(error, files) {
+    
+    filenames = files.filter(function(f) {
+      return f.match(/.*TIF/);
+    });
+    
+    for (var i = 0; i < filenames.length; i += 1) {
+      var filename = filenames[i];
+      
+      var srcpath = path.join(datadir, filename);
+      var dstpath = path.join(datadir, 'webmercator', filename);
+      var ctrlpath = path.join(datadir, 'control', filename);
+      
+      wmtiff.reproject(srcpath, dstpath, function(error) {
+        
+        var src = gdal.open(dstpath);
+        var ctrl = gdal.open(ctrlpath);
+        
+        for (var j = 0; i < src.geoTransform.length; i += 1) {
+          assert.equal(src.geoTransform[i], ctrl.geoTransform[i]);
+        }
+        
+      });
+    }
+    
+    
+    assert.end();
+  });
+  
 });
 
 
 tape('teardown', function (test) {
+  
+  var dir = path.join(__dirname, 'fixtures', 'webmercator');
+  fs.rmdir(dir, function(error) {
+    test.end();
+  });
+  
 });
