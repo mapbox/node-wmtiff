1cc89f04cff15254c42b4f6b773fec3e90095c3d
Ryan Clark <ryan.clark.j@gmail.com>
2015-01-31 16:12:04 -0700

    fuzzy equality check

--- test/test.js
+++ test/test.js
@@ -5,57 +5,62 @@ var tape = require('tape');
 var wmtiff = require('../index.js');
 var gdal = require('gdal');
 
+function equalEnough(assert, found, expected, message) {
+  found = Math.floor(found * Math.pow(10, 6)) / Math.pow(10, 6);
+  expected = Math.floor(expected * Math.pow(10, 6)) / Math.pow(10, 6);
+  assert.equal(found, expected, message);
+}
 
 tape('setup', function(assert) {
-  
+
   var dir = path.join(__dirname, 'fixtures', 'webmercator');
   fs.mkdir(dir, function(error) {
     assert.end();
   });
-  
+
 });
 
 tape('reproject', function(assert) {
-  
+
   var datadir = path.join(__dirname, 'fixtures');
   fs.readdir(datadir, function(error, files) {
-    
+
     filenames = files.filter(function(f) {
       return f.match(/.*TIF/);
     });
-    
+
     for (var i = 0; i < filenames.length; i += 1) {
       var filename = filenames[i];
-      
+
       var srcpath = path.join(datadir, filename);
       var dstpath = path.join(datadir, 'webmercator', filename);
       var ctrlpath = path.join(datadir, 'control', filename);
-      
+
       wmtiff.reproject(srcpath, dstpath, function(error) {
-        
+
         var src = gdal.open(dstpath);
         var ctrl = gdal.open(ctrlpath);
-        
+
         for (var j = 0; i < src.geoTransform.length; i += 1) {
-          assert.equal(src.geoTransform[i], ctrl.geoTransform[i]);
+          equalEnough(assert, src.geoTransform[i], ctrl.geoTransform[i]);
         }
-        
+
         fs.unlink(dstpath);
       });
     }
-    
-    
+
+
     assert.end();
   });
-  
+
 });
 
 
 tape('teardown', function (test) {
-  
+
   var dir = path.join(__dirname, 'fixtures', 'webmercator');
   fs.rmdir(dir, function(error) {
     test.end();
   });
-  
+
 });
